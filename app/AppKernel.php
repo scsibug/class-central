c8f17c394fa35c7847cd31beee3f5dc10d14708d
Dhawal Shah <dhawalhshah@gmail.com>
2011-11-24 00:03:44 -0600

    Added doctrine migration to the path

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new ClassCentral\StaticBundle\ClassCentralStaticBundle(),
             new ClassCentral\SiteBundle\ClassCentralSiteBundle(),
+            new Symfony\Bundle\DoctrineMigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
