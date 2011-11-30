f99dec4d830baf5e0d8e8b20a4a8fd50e9d26c41
Dhawal Shah <dhawalhshah@gmail.com>
2011-11-29 18:49:04 -0600

    Removed the default acme bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,8 +22,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\DoctrineMigrationsBundle\DoctrineMigrationsBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {          
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
